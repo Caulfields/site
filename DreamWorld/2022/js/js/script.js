76f1a7ec07429450636dccb80ff720f3d2925087
Caulfields <igory345@gmail.com>
2022-10-24 15:28:37 +0500

    dfdf

--- DreamWorld/2022/js/js/script.js
+++ DreamWorld/2022/js/js/script.js
@@ -1,6 +1,11 @@
 
-let userInfo = {
-	name: "Loop",
-	age: 34,
+const textElement = document.querySelector ('.text');
+const textElementContest = textElement.innerHTML;
 
+const buttclick = document.querySelector ('.butt');
+
+buttclick.onclick = function () {
+	console.log('Click!');
 };
+
+textElement.innerHTML = `<p>${textElementContest}<p> Ты красава`;
